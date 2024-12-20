8340ba46b76aff3559ebacc838790ff485b0747c
Adriansyah Ravindra <59479003+adrnsyhrvndra@users.noreply.github.com>
2024-12-20 20:55:36 +0700

    Design Styling Tambah dan Edit Categorie

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -1,48 +1,100 @@
-<!DOCTYPE html>
-<html>
-<head>
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
-      <!-- Custom Local Font -->
-      <link rel="preconnect" href="https://fonts.googleapis.com">
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-      <link href="https://fonts.googleapis.com/css2?family=Rethink+Sans:ital,wght@0,400..800;1,400..800&display=swap" rel="stylesheet">
-      <style>
-            body {
-                  font-family: 'Rethink Sans', sans-serif;
-            }
-      </style>
-      <title>Form Tambah Categorie</title>
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
-      <script src="{{ asset('assets/jquery.js') }}"></script>
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
-</head>
-<body style="padding-bottom: 100px">
-      <div class="container-fluid">
-            <div class="row">
-                <div class="col-12 w-full" style="background-image: url('https://images.unsplash.com/photo-1576091160399-112ba8d25d1d?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'); background-size: cover; background-position: center; background-repeat: no-repeat;">
-                    <div class="text-center">
-                        <h4 class="text-white text-center fs-1" style="padding-top: 130px; padding-bottom: 130px;">Tambah Data Category</h4>
+@extends('layouts.admin')
+
+@section('title', 'Halaman Tambah Categorie')
+
+@section('content')
+
+    <div id="layout-wrapper">
+        @include('layouts.partials.headerAdmin')
+        @include('layouts.partials.sidebarAdmin')
+        <div class="main-content">
+            <div class="page-content">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="page-title-box d-sm-flex align-items-center justify-content-between">
+                                <h4 class="mb-sm-0 font-size-18">Halaman Data Categorie</h4>
+                                <div class="page-title-right">
+                                    <ol class="breadcrumb m-0">
+                                        <li class="breadcrumb-item"><a href="/admin">Dashboard</a></li>
+                                        <li class="breadcrumb-item"><a href="/categorie">Categorie</a></li>
+                                        <li class="breadcrumb-item active">Tambah Categorie</li>
+                                    </ol>
+                                </div>
+                            </div>
+                        </div>
                     </div>
+                    <form action="/categorie/store" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="card">
+                                    <div class="card-header">
+                                        <h4 class="card-title">Tambah Data Categorie</h4>
+                                        <p class="card-title-desc">
+                                            Ayo tambah data categorie disini untuk memudahkan penjualan anda.
+                                        </p>
+                                    </div>
+                                    <div class="card-body">
+                                        <div class="row">
+                                                <div class="col-6">
+                                                      <div class="mb-3">
+                                                            <label for="category_name" class="form-label">Categorie Name</label>
+                                                            <input class="form-control" type="text" name="category_name" id="category_name" required>
+                                                      </div>
+                                                </div>
+                                        </div>                                    
+                                        <div class="row justify-content-end mb-3 mt-2">
+                                            <div class="col-6 text-end">
+                                                <a href="/categorie" class="btn btn-danger waves-effect waves-light w-lg">
+                                                    <i class="mdi mdi-keyboard-backspace font-size-16 align-middle"></i>
+                                                    Kembali
+                                                </a>
+                                                <button type="submit" class="btn btn-success waves-effect waves-light w-lg">
+                                                    <i class="mdi mdi-checkbox-marked-circle-plus-outline font-size-16 align-middle"></i>
+                                                    Tambah Data Categorie
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
-      </div>
-      <div class="container-fluid">
-            <div class="row justify-content-center" style="margin-top:13%">
-                  <div class="col-8">
-                        <h4>Form Tambah Category</h4>
-                        <form action="/categorie/store" method="POST" enctype="multipart/form-data">
-                              @csrf
-                              <div class="form-group">
-                                    <label for="category_name">Category Name</label>
-                                    <input type="text" name="category_name" class="form-control" id="category_name" />
-                              </div>
-                              <div style="text-align:center">
-                                    <button type="submit" class="btn btn-success">Simpan</button>
-                                    <a href="/categorie" class="btn btn-danger">Kembali</a>
-                              </div>
-                        </form>
-                  </div>
-            </div>
-      </div>
-</body>
-</html>
+            @include('layouts.partials.footerAdmin')
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script>
+
+        Dropzone.autoDiscover = false;
+        const dropzone = new Dropzone("#dropzone-area", {
+            url: "/Categorie/upload-logo",
+            maxFiles: 1,
+            acceptedFiles: "image/*",
+            addRemoveLinks: true,
+            dictDefaultMessage: "Drop files here or click to upload.",
+        });
+
+        @if (session('success'))
+            Swal.fire({
+                icon: 'success',
+                title: 'Berhasil!',
+                text: "{{ session('success') }}"
+            });
+        @endif
+
+        @if (session('error'))
+            Swal.fire({
+                icon: 'error',
+                title: 'Gagal!',
+                text: "{{ session('error') }}"
+            });
+        @endif
+    </script>
+
+@endsection
